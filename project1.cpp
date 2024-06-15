68383980055608e5419b1cb21e33fd521e260940
Hawkeye Barton <hawkeye.business09@hotmail.com>
2024-06-15 13:37:49 +0300

    Proje dosyası ekle.

---
+++ project1.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int age;
+    string driversLicense, universityGraduate;
+
+    cout << "Please enter your ag': ";
+    cin >> age;
+    cout << endl;
+
+    if (age >= 40)
+    {
+        cout << "We're sorry, you do not meet our criteria. You need to be under 40 years old. ";
+    }
+    else
+    {
+        cout << "Do you have a driver's license? (Yes / No) ";
+        cin >> driversLicense;
+        cout << endl;
+        if (driversLicense == "Yes" || driversLicense == "yes")
+        {
+            cout << "Are you a university graduate? (Yes / No) ";
+            cin >> universityGraduate;
+
+            if (universityGraduate == "Yes" || universityGraduate == "yes")
+            {
+                cout << endl << "Congratulations, you have been hired. " << endl;
+            }
+            else
+            {
+                cout << endl << "We're sorry, you need to be a university graduate. " << endl;
+            }
+        }
+        else
+        {
+            cout << endl << "We're sorry, your application was rejected because you do not have a driver's license. " << endl;
+        }
+    }
+
+    cout << endl;
+
+    return 0;
+}
